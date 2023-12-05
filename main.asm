d9122bb9ffaefd2b49871d6da191deb2edae5ef7
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2023-12-05 14:37:37 -0300

    feat: movendo asteroides

--- main.asm
+++ main.asm
@@ -51,7 +51,7 @@
     KEY_ENTER   EQU 28
     KEY_W       EQU 11h
     KEY_S       EQU 1Fh
-    
+
     BAR_STEP_SIZE    EQU 10
     
     TARGET_FRAMERATE EQU 20
@@ -107,7 +107,34 @@
                        0  ,0  ,0  ,0  ,2  ,2  ,0  ,0  ,0  ,0, \
                        0  ,0  ,0  ,0  ,2  ,2  ,0  ,0  ,0  ,0
 
-                       
+    ;SPRITE_PRETO   DB 0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0, \
+    ;                   0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0
+
+    ; Sprite com borda branca para debugar                  
+    SPRITE_PRETO   DB 15  ,15  ,15  ,15  ,15  ,15  ,15  ,15  ,15  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,15, \
+                    15  ,15  ,15  ,15  ,15  ,15  ,15  ,15  ,15  ,15
+    SPRITE_WIDTH EQU 10
+    SPRITE_HEIGHT EQU 10
+
+    ; Constantes do jogo                  
+    NAVE_SPEED  EQU 2
+    MAX_ASTEROIDES  EQU 8
                        
                        
                        
@@ -115,17 +142,11 @@
     ; Estado do jogo
     nroVidas        DB 5
     tempoRestante   DW 30  
-    
-    naveX dw 30
-    naveY dw 80
-
-    naveXAnterior dw 0
-    naveYAnterior dw 0
-    
-    NAVE_SPEED  EQU 2
+    naveX           DW 30
+    naveY           DW 80
+    nivelAtual      DB 1
     
     ASTEROIDES      DW 0,0,0,0,0,0,0,0
-    MAX_ASTEROIDES  EQU 8 
           
 .code
 
@@ -321,30 +342,32 @@ PINTA_LINHA proc
 endp
 
 ;Desenha o elemento em SI (com 10x10) em AX 
-DESENHA_10x10 proc
+DESENHA_SPRITE proc
+    push CX
+    push DX
+    push BX
     push SI
     push DI
-    push BX
-    push DX
     
-    mov DI, AX ;coordenada
+    mov DI, AX
 
-    mov BX, 10
-    LOOP_COLUNAS:
-        mov CX, 10
-        
-        rep movsb        
+    mov BX, SPRITE_HEIGHT    
+    __DESENHA_LINHA_SPRITE:
+
+        mov CX, SPRITE_WIDTH
+        rep movsb
         
         dec BX
-        add DI, SCREEN_WIDTH - 10
-        
+        add DI, SCREEN_WIDTH - SPRITE_WIDTH
+
         cmp BX, 0
-        jne LOOP_COLUNAS
-    
-    pop DX
-    pop BX
+        jne __DESENHA_LINHA_SPRITE
+
     pop DI
     pop SI
+    pop BX
+    pop DX
+    pop CX
     ret
 endp
 
@@ -365,6 +388,27 @@ DESENHA_LINHA proc
     pop CX
     ret
 endp
+
+;ax = coordenada inicial
+;bx = quantidade pixels
+LIMPA_COLUNA_PIXELS proc
+    push CX
+    push DI
+    
+    mov DI, AX
+    mov CX, BX
+    mov AX, 0
+    
+    LOOP_LIMPA_COLUNA:
+        stosb
+        add DI, SCREEN_WIDTH 
+        loop LOOP_LIMPA_COLUNA
+    
+    pop DI
+    pop CX
+    ret
+endp
+
 
 RENDER_INTERFACE proc
     push AX
@@ -478,6 +522,90 @@ ATUALIZA_BARRA_VIDA proc
     ret
 endp
 
+MOVE_ASTEROIDES proc
+    push CX
+    push SI
+    push AX
+    push BX
+    push DX
+    
+    mov CX, MAX_ASTEROIDES
+    mov SI, offset ASTEROIDES
+    
+    ; Calcula a velocidade do asteroide
+    ; DX = nivelAtual * NAVE_SPEED
+    xor BX, BX
+    mov AL, nivelAtual
+    mov BL, NAVE_SPEED
+    mul BL
+    mov DX, AX
+
+    __VERIFICA_ASTEROIDES_EXISTENTES:
+        mov AX, [SI]
+        cmp AX, 0 ; Se existe o asteroide, move ele
+        jne __MOVE_ASTEROIDE
+        
+        __CONTINUA_VERIFICA_ASTEROIDES_EXISTENTES:
+            add SI, WORD_SIZE ; Proximo asteroide
+            loop __VERIFICA_ASTEROIDES_EXISTENTES
+
+    jmp FIM_MOVE_ASTEROIDES
+    
+    __MOVE_ASTEROIDE:
+        push SI
+        ; AX = Coordenada do asteroide
+        
+        ;1 - Limpa o asteroide
+        mov SI, offset SPRITE_PRETO
+        call DESENHA_SPRITE
+
+        ;2 - Move o asteroide
+        sub AX, DX
+
+        ;3 - Verifica se o asteroide saiu da tela
+        push AX
+        push BX
+        push DX
+
+        ; Se AX % 320 == 0, o asteroide saiu da tela
+        xor DX,DX 
+        mov BX, SCREEN_WIDTH
+        div BX
+        
+        cmp DX, 0
+
+        pop DX
+        pop BX
+        pop AX
+
+        je __DESTROI_ASTEROIDE
+
+
+        ;4 - Desenha o asteroide na nova posição
+        mov SI, offset SPRITE_METEORO
+        call DESENHA_SPRITE
+
+        pop SI
+        mov [SI], AX ; Salva a nova posição do asteroide
+
+        jmp __CONTINUA_VERIFICA_ASTEROIDES_EXISTENTES
+
+    jmp FIM_MOVE_ASTEROIDES
+    __DESTROI_ASTEROIDE:
+        pop SI
+        mov BX, 0
+        mov [SI], BX 
+        jmp __CONTINUA_VERIFICA_ASTEROIDES_EXISTENTES
+
+    FIM_MOVE_ASTEROIDES:
+        pop DX
+        pop BX
+        pop AX
+        pop SI
+        pop CX
+    ret
+endp
+
 GERA_ASTEROIDE proc
     push AX
     push DX
@@ -487,25 +615,28 @@ GERA_ASTEROIDE proc
     
     mov CX, MAX_ASTEROIDES
     mov SI, offset ASTEROIDES
+
+    ; Verifica se existem asteroides a serem gerados
     LOOP_VERIFICA_ASTEROIDE:
         mov AX, [SI]
-        cmp AX, 0 
-        je GERA_ASTEROIDE_EFET
-        add SI, WORD_SIZE
+        cmp AX, 0 ; Se 0, espaço do asteroide está vazio
+        je __GERA_ASTEROIDE
+
+        add SI, WORD_SIZE; Proximo asteroide
         loop LOOP_VERIFICA_ASTEROIDE
     
     jmp FIM_GERA_ASTEROIDE
     
-    GERA_ASTEROIDE_EFET:
+    __GERA_ASTEROIDE:
         mov BX, SI
-        mov AX, 6400
+        mov AX, SCREEN_WIDTH * 18
         call RAND_NUMBER
         
         mul DX
-        add AX, SCREEN_WIDTH - 20
+        add AX, SCREEN_WIDTH * 2 - 30 ; Move o asteoide para o lado direito da tela
         
         mov SI, offset SPRITE_METEORO
-        call DESENHA_10x10
+        call DESENHA_SPRITE
         
         mov SI, BX
         mov [SI], AX
@@ -534,25 +665,15 @@ INICIAR_JOGO proc
 
         call ATUALIZA_BARRA_VIDA
         call ATUALIZA_BARRA_TEMPO
-        
-        call MOVE_NAVE
-        call LIMPA_AREA_NAVE 
-        
-        mov AX, naveY
-        mov CX, SCREEN_WIDTH
-        mul CX
-        add AX, naveX
-        mov DI, AX  
+        call ATUALIZA_NAVE
 
-        mov SI, offset SPRITE_NAVE
-
-        call DESENHA_10x10 
         call SLEEP 
+        call MOVE_ASTEROIDES
         
         inc BX
         cmp BX, TARGET_FRAMERATE
         je ATUALIZA_TEMPO_RESTANTE
-        
+
         jmp MAIN
     
     ATUALIZA_TEMPO_RESTANTE:        
@@ -567,45 +688,94 @@ INICIAR_JOGO proc
     ret
 endp
 
-MOVE_NAVE proc
+ATUALIZA_NAVE proc
     push AX
     push BX
     push CX
     push DX
     
-    mov AX, naveX
-    mov naveXAnterior, AX
+    call _MOVE_NAVE
     mov AX, naveY
-    mov naveYAnterior, AX
+    mov CX, SCREEN_WIDTH
+    mul CX
+    add AX, naveX
+    mov DI, AX  
+    mov SI, offset SPRITE_NAVE
+    call DESENHA_SPRITE 
+
+
+    pop DX
+    pop CX
+    pop BX
+    pop AX
+    ret
+endp
+
+_MOVE_NAVE proc
+    push AX
+    push BX
+    push CX
+    push DX
+    
+    push naveX
+    push naveY
     
     mov AH, 1
     int 16h
-    jz FIM_MOVE_NAVE
+    jz __MOVE_NAVE_FIM
 
     mov AH, 0
     int 16h
 
     cmp AH, KEY_W
-    je MOVE_PARA_CIMA
+    je __MOVER_CIMA
     
     cmp AH, KEY_S 
-    je MOVE_PARA_BAIXO
+    je __MOVER_BAIXO
 
-    jmp FIM_MOVE_NAVE
 
-    MOVE_PARA_CIMA:
+    jmp __MOVE_NAVE_FIM
+
+    __MOVER_CIMA:
         sub naveY, NAVE_SPEED
-        jmp FIM_MOVE_NAVE
+        jmp __MOVE_NAVE_FIM
 
-    MOVE_PARA_BAIXO:
+    __MOVER_BAIXO:
         add naveY, NAVE_SPEED
+        ;jmp __MOVE_NAVE_FIM
+    __MOVE_NAVE_FIM:
 
-    FIM_MOVE_NAVE:
+        ; Limpa a posição anterior da nave
+        pop AX ; NaveY
+        pop BX ; NaveX
+        call CONVERTE_XY
+
+        push SI
+        mov SI, offset SPRITE_PRETO
+        call DESENHA_SPRITE
+
+        pop SI
         pop DX
         pop CX
         pop BX
         pop AX
         ret
+endp
+
+; Converte posicao X,Y -> offset
+; AX = X
+; BX = Y
+CONVERTE_XY proc
+    push CX
+    push DX
+
+    mov CX, SCREEN_WIDTH
+    mul CX
+    add AX, BX
+
+    pop DX
+    pop CX
+    ret
 endp
 
 
@@ -633,37 +803,6 @@ LIMPAR_TELA proc
     ret
 endp
 
-LIMPA_AREA_NAVE proc
-    push AX
-    push BX
-    push CX
-    push DX
-    push DI
-
-    mov AX, naveYAnterior
-    mov CX, SCREEN_WIDTH
-    mul CX
-    add AX, naveXAnterior
-    mov DI, AX
-
- 
-    mov CX, 10
-    LIMPA_LINHA:
-        push CX
-        mov CX, 10
-        mov AX, COLOR_BLACK
-        rep stosb
-        pop CX
-        add DI, SCREEN_WIDTH - 10 ; Proxima linha
-        loop LIMPA_LINHA
-
-    pop DI
-    pop DX
-    pop CX
-    pop BX
-    pop AX
-    ret
-LIMPA_AREA_NAVE endp
 
 ACAO_MENU proc
     push AX
